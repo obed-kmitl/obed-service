d701d88e26a087735acce1b06e212f6ae70ba07a
noppanat.pa <noppanat.pa@easysunday.com>
2021-12-30 22:04:41 +0700

    update token expire time configuration

--- api/configs/auth.ts
+++ api/configs/auth.ts
@@ -9,8 +9,8 @@ const authConfig = {
 	development: {
 		salt: Number(env.AUTH_SALT),
 		secret: env.AUTH_SECRET,
-		jwtExpiration: 5, // 180 second
-		jwtRefreshExpiration: 20, // 7 days
+		jwtExpiration: 180, // 180 second
+		jwtRefreshExpiration: 7, // 7 days
 	},
 };
 
