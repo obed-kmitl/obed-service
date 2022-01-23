a786685d323555e867d6577bddc51b09ed41c127
Noppanat Panlutan <architon1997@gmail.com>
2022-01-24 00:51:10 +0700

    migrate clo service

---
+++ migrations/sqls/20220123174427-create-clo-up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE clos(
+	clo_id SERIAL PRIMARY KEY,
+	section_id INT NOT NULL REFERENCES sections(section_id) ON DELETE CASCADE,
+  detail VARCHAR(300),
+  order_number SMALLINT NOT NULL
+);
+
+CREATE TABLE clo_sub_standards(
+	clo_id INT NOT NULL REFERENCES clos(clo_id) ON DELETE CASCADE,
+	map_sub_std_id INT REFERENCES map_sub_standards(map_sub_std_id) ON DELETE CASCADE,
+  PRIMARY KEY (clo_id, map_sub_std_id)
+);
+
+CREATE TABLE sub_activities_clo(
+  sub_activity_id INT NOT NULL REFERENCES sub_activities(sub_activity_id) ON DELETE CASCADE,
+  clo_id INT NOT NULL REFERENCES clos(clo_id) ON DELETE CASCADE,
+  PRIMARY KEY (sub_activity_id, clo_id)
+);
