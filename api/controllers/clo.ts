4dd4197f47d791a7aee1a9c4ec16a98db25d2703
Noppanat Panlutan <architon1997@gmail.com>
2022-02-02 00:39:03 +0700

    create clo - fix bug cannot create

--- api/controllers/clo.ts
+++ api/controllers/clo.ts
@@ -7,11 +7,28 @@ import { Request, Response } from 'express';
  * Create Course Learning Outcome
  */
 const create = async (req: Request, res: Response): Promise<Response> => {
-	const result = await cloRepository.create(req.body);
+	const { relative_standards, ...cloInput } = req.body;
+
+	const result = await cloRepository.create(cloInput);
 
 	const { clo_id: cloId } = result.rows[0];
 
-	const { rows: [clo] } = await cloRepository.find(cloId);
+	const curriculumId = await cloRepository.findCurriculumByCLO(cloId);
+
+	let mapStandards = [];
+	if (relative_standards.length > 0) {
+		const resultMapstandard = await mapStandardRepository.findMapStandardBySubStdId(
+			relative_standards, curriculumId,
+		);
+		mapStandards = resultMapstandard.rows.map((row) => row.map_sub_std_id);
+	}
+
+	const relativeStandardInfo = mapStandards.map((map_sub_std_id) => ([
+		cloId,
+		map_sub_std_id,
+	]));
+
+	const { rows: [clo] } = await cloRepository.createCLOSubStandards(cloId, relativeStandardInfo);
 
 	const filterDuplicateRelative = clo.relative_sub_standards.filter((
 		value, index, self,
