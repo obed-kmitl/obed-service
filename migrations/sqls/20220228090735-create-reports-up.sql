74ecfc5a871f82be5dc7622207f38f56e69a38f0
Noppanat Panlutan <architon1997@gmail.com>
2022-02-28 16:16:19 +0700

    report - create migration

---
+++ migrations/sqls/20220228090735-create-reports-up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE reports(
+	report_id SERIAL PRIMARY KEY,
+  section_id INT REFERENCES sections(section_id) ON DELETE CASCADE,
+  grade INT[8],
+  prev_improvement VARCHAR(300)[],
+  verify_method VARCHAR(300)[],
+  summary VARCHAR(300)[]
+);
+
+CREATE TABLE next_improvements(
+	next_improve_id SERIAL PRIMARY KEY,
+  report_id INT REFERENCES reports(report_id) ON DELETE CASCADE,
+  title VARCHAR(300),
+  cause VARCHAR(300)[],
+  work VARCHAR(300)[],
+  evaluation VARCHAR(300)[]
+);
