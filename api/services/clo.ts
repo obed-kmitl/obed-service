901e0db755e8aa01b096a6fefc0cd301f135bf89
Noppanat Panlutan <architon1997@gmail.com>
2022-03-18 00:49:45 +0700

    fix bug

--- api/services/clo.ts
+++ api/services/clo.ts
@@ -5,7 +5,7 @@ export const getBySection = async (sectionId: number) => {
 	const cloResult = await cloRepository.findAllBySection(sectionId);
 	return cloResult.rows.map((el) => ({
 		...el,
-		main_sub_standards: el.main_sub_standards.map((mss) => `${mss.group_sub_order_number}.${mss.sub_order_number}`),
-		relative_sub_standards: el.main_sub_standards.map((rss) => `${rss.group_sub_order_number}.${rss.sub_order_number}`),
+		main_sub_standards: _.uniq(el.main_sub_standards.map((mss) => `${mss.group_sub_order_number}.${mss.sub_order_number}`)),
+		relative_sub_standards: _.uniq(el.relative_sub_standards.map((rss) => `${rss.group_sub_order_number}.${rss.sub_order_number}`)),
 	}));
 };
